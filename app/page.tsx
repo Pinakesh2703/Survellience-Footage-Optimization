218611e48c03550154b71f8f8dc35751a5e7d81d
Pinakesh2703 <pinakesh.chatto2016@gmail.com>
2024-03-06 22:41:03 +0530

    updated buttons 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useState } from 'react';
+"use client"
+import React, { useRef, useState } from 'react'
 import Webcam from 'react-webcam';
 import './customstyle.css';
 import { Separator } from '@/components/ui/separator';
@@ -6,58 +7,55 @@ import { ModeToggle } from '@/components/theme-toggle';
 import Button from '@material-ui/core/Button';
 import { Camera, CameraIcon, VideoIcon } from 'lucide-react';
 
-type Props = {};
+type Props = {}
 
-const Page = (props: Props) => {
+
+const Page = (Props: Props) => {
   const webcamRef = useRef<Webcam>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null); 
 
-  // State
-  const [isRecording, setIsRecording] = useState<boolean>(false);
+  //state
+
+  const [isRecording, setIsRecording]=useState<boolean>(false);
 
   return (
     <body>
-      <button></button>
+      <button>
+
+      </button>
       <div className='header'>
         Header content goes here
       </div>
-      <div className='cambox'>
-        <Webcam ref={webcamRef} mirrored className='webcamclass' />
-        <canvas ref={canvasRef} className='canvasclass'></canvas>
-      </div>
-      <Separator className='separator' />
-      <ModeToggle />
-      <div className='container'>
-        <Button variant="contained" color="primary" className='box' onClick={userPromptScreenshot}>
-          <CameraIcon />Capture
-        </Button>
-        <Button variant={isRecording ? 'destructive' : 'outlined'} color="primary" className='box' onClick={userPromptRecord}>
-  <VideoIcon />Auto Record
-</Button>
+        <div className='cambox'>
+          <Webcam ref={webcamRef} mirrored className='webcamclass' />
+          <canvas ref={canvasRef}
+          className='canvasclass'></canvas>
+        </div>
+        <Separator className='separator'/>
+        <ModeToggle></ModeToggle>
 
-        <Button variant="contained" color="primary" className='box' onClick={handleManualRecord}>
-          <VideoIcon />Manual Record
-        </Button>
-      </div>
-      <Separator className='separator' />
-      <div className='footer'>
+        <div className='container'>
+          <button  className='box'  onClick={userPromptScreenshot}><CameraIcon/>Capture</button>
+          <button className='box'  onClick={userPromptRecord}><VideoIcon/>Auto Record</button>
+          {/* <button className='box'  onClick={}><VideoIcon/>manual record</button> */}
+        </div>
+
+        <Separator className='separator'/>
+        <div className='footer'>
         Footer content goes here
       </div>
     </body>
+    
   );
-
-  function userPromptScreenshot() {
+  function userPromptScreenshot(){
     //add screenshot 
+
     //save it to downloads
   }
-
-  function userPromptRecord() {
+  function userPromptRecord(){
     // record 
-    // save
-  }
 
-  function handleManualRecord() {
-    // handle manual record
+    // save
   }
 };
 

9d546b62d7b98a6c08de5aa8dd9b0db8ee698fa2
AntareepDey <antareep175@hotmail.com>
2024-04-30 12:09:25 +0530

    final review

--- app/page.tsx
+++ app/page.tsx
@@ -1,183 +1,400 @@
 "use client"
-import React, { use, useEffect, useRef,useState } from 'react'
-import Webcam from 'react-webcam';
-import './customstyle.css';
-import { Separator } from '@/components/ui/separator';
 import { ModeToggle } from '@/components/theme-toggle';
-import { Camera, CameraIcon, PersonStanding, VideoIcon, Volume2Icon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { toast } from "sonner"
-import { Puff, Rings} from 'react-loader-spinner';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Separator } from '@/components/ui/separator';
 import { Slider } from '@/components/ui/slider';
+import { beep } from '@/utils/audio';
+import { Camera, Divide, FlipHorizontal, MoonIcon, PersonStanding, SunIcon, Video, Volume2 } from 'lucide-react';
+import React, { useEffect, useRef, useState } from 'react'
+import { Rings } from 'react-loader-spinner';
+import Webcam from 'react-webcam';
+import { toast } from "sonner"
 import * as cocossd from '@tensorflow-models/coco-ssd'
 import "@tensorflow/tfjs-backend-cpu"
 import "@tensorflow/tfjs-backend-webgl"
 import { DetectedObject, ObjectDetection } from '@tensorflow-models/coco-ssd';
-import StaticGenerationSearchParamsBailoutProvider from 'next/dist/client/components/static-generation-searchparams-bailout-provider';
 import { drawOnCanvas } from '@/utils/draw';
+import SocialMediaLinks from '@/components/social-links';
 
 type Props = {}
 
-let interval :any =null;
-const Page = (Props: Props) => {
+let interval: any = null;
+let stopTimeout: any = null;
+const HomePage = (props: Props) => {
   const webcamRef = useRef<Webcam>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null); 
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  // state 
+  const [mirrored, setMirrored] = useState<boolean>(true);
   const [isRecording, setIsRecording] = useState<boolean>(false);
-  const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false);
-  const  [volume,setvolume]= useState(0);
-  const [model,setModel]=useState<ObjectDetection>();
+  const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false)
+  const [volume, setVolume] = useState(0.8);
+  const [model, setModel] = useState<ObjectDetection>();
   const [loading, setLoading] = useState(false);
-  
-  
-  
+
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+
+  // initialize the media recorder
+  useEffect(() => {
+    if (webcamRef && webcamRef.current) {
+      const stream = (webcamRef.current.video as any).captureStream();
+      if (stream) {
+        mediaRecorderRef.current = new MediaRecorder(stream);
+
+        mediaRecorderRef.current.ondataavailable = (e) => {
+          if (e.data.size > 0) {
+            const recordedBlob = new Blob([e.data], { type: 'video' });
+            const videoURL = URL.createObjectURL(recordedBlob);
+
+            const a = document.createElement('a');
+            a.href = videoURL;
+            a.download = `${formatDate(new Date())}.webm`;
+            a.click();
+          }
+        };
+        mediaRecorderRef.current.onstart = (e) => {
+          setIsRecording(true);
+        }
+        mediaRecorderRef.current.onstop = (e) => {
+          setIsRecording(false);
+        }
+      }
+    }
+  }, [webcamRef])
+
+
   useEffect(() => {
     setLoading(true);
     initModel();
-    }, [model])
-    
+  }, [])
+
+  // loads model 
+  // set it in a state varaible
   async function initModel() {
-      const loadedModel: ObjectDetection = await cocossd.load({
-        base: 'lite_mobilenet_v2'
-      });   
-      setModel(loadedModel);
-  };
-      
+    const loadedModel: ObjectDetection = await cocossd.load({
+      base: 'lite_mobilenet_v2'
+    });
+    setModel(loadedModel);
+  }
+
   useEffect(() => {
-      if(model){
-        setLoading(false);
-      }
-    
+    if (model) {
+      setLoading(false);
+    }
   }, [model])
+
+
+  async function runPrediction() {
+    if (
+      model
+      && webcamRef.current
+      && webcamRef.current.video
+      && webcamRef.current.video.readyState === 4
+    ) {
+      const predictions: DetectedObject[] = await model.detect(webcamRef.current.video);
+
+      resizeCanvas(canvasRef, webcamRef);
+      drawOnCanvas(mirrored, predictions, canvasRef.current?.getContext('2d'))
+
+      let isPerson: boolean = false;
+      if (predictions.length > 0) {
+        predictions.forEach((prediction) => {
+          isPerson = prediction.class === 'person';
+        })
+
+        if (isPerson && autoRecordEnabled) {
+          startRecording(true);
+        }
+      }
+    }
+  }
 
   useEffect(() => {
     interval = setInterval(() => {
       runPrediction();
-    },100)
-    return () => {
-      clearInterval(interval);
-    }
-  },[webcamRef.current,model])
+    }, 100)
 
-  async function runPrediction(){
-    if(webcamRef.current && model && webcamRef.current.video && webcamRef.current.video.readyState === 4){
-      const predictions: DetectedObject[]= await model.detect(webcamRef.current.video)
-      resizeCanvas(canvasRef,webcamRef);  
-      drawOnCanvas(predictions,canvasRef.current?.getContext('2d'));
-    
-    }
-  
-  }
+    return () => clearInterval(interval);
+  }, [webcamRef.current, model, mirrored, autoRecordEnabled, runPrediction])
 
   return (
-    <body>
-        <div className='header'>
-        <div className="headerLeft">SURVELLIENCE FOOTAGE OPTIMIZATION</div>
-          <div className='headerRight'><ModeToggle></ModeToggle></div>     
+
+    <div className='flex h-screen'>
+      {/* Left division - webcam and Canvas  */}
+      <div className='relative'>
+        <div className='relative h-screen w-full'>
+          <Webcam ref={webcamRef}
+            mirrored={mirrored}
+            className='h-full w-full object-contain p-2'
+          />
+          <canvas ref={canvasRef}
+            className='absolute top-0 left-0 h-full w-full object-contain'
+          ></canvas>
+        </div>
+      </div>
+
+      {/* Righ division - container for buttion panel and wiki secion  */}
+      <div className='flex flex-row flex-1'>
+        <div className='border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between shadow-md rounded-md p-4'>
+          {/* top secion  */}
+          <div className='flex flex-col gap-2'>
+            <ModeToggle />
+            <Button
+              variant={'outline'} size={'icon'}
+              onClick={() => {
+                setMirrored((prev) => !prev)
+              }}
+            ><FlipHorizontal /></Button>
+
+            <Separator className='my-2' />
+          </div>
+
+          {/* Middle section  */}
+          <div className='flex flex-col gap-2'>
+            <Separator className='my-2' />
+            <Button
+              variant={'outline'} size={'icon'}
+              onClick={userPromptScreenshot}
+            >
+              <Camera />
+            </Button>
+            <Button
+              variant={isRecording ? 'destructive' : 'outline'} size={'icon'}
+              onClick={userPromptRecord}
+            >
+              <Video />
+            </Button>
+            <Separator className='my-2' />
+            <Button
+              variant={autoRecordEnabled ? 'destructive' : 'outline'}
+              size={'icon'}
+              onClick={toggleAutoRecord}
+            >
+              {autoRecordEnabled ? <Rings color='white' height={45} /> : <PersonStanding />}
+
+            </Button>
+          </div>
+
+          <div className='flex flex-col gap-2'>
+            <Separator className='my-2' />
+
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant={'outline'} size={'icon'}>
+                  <Volume2 />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <Slider
+                  max={1}
+                  min={0}
+                  step={0.2}
+                  defaultValue={[volume]}
+                  onValueCommit={(val) => {
+                    setVolume(val[0]);
+                    beep(val[0]);
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
         </div>
 
-        
-          <div className='cambox'>
-          <Webcam
-              ref={webcamRef}
-              mirrored={true}
-              style={{ height: '100%', width: '100%', objectFit: 'contain', padding: '0.5rem' }}
-              className='custom-webcam'
-            />
-            <canvas
-              ref={canvasRef}
-              style={{ position: 'absolute', top: '0', left: '0', height: '100%', width: '100%', objectFit: 'contain', transform: 'scaleX(-1)', padding: '0.5rem'}}
-              className='custom-canvas'
-              
-            ></canvas>
+        <div className='h-full flex-1 py-4 px-2 overflow-y-scroll'>
+          <RenderFeatureHighlightsSection />
+        </div>
+      </div>
+      {loading && <div className='z-50 absolute w-full h-full flex items-center justify-center bg-primary-foreground'>
+        Getting things ready . . . <Rings height={50} color='red' />
+      </div>}
+    </div>
+  )
 
-          </div>
-  
-          
+  // handler functions
 
-          <Separator className='separator'/>
-          
+  function userPromptScreenshot() {
 
-          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} className='ButtonParent'>
+    // take picture
+    if(!webcamRef.current){
+      toast('Camera not found. Please refresh');
+    }else{
+      const imgSrc = webcamRef.current.getScreenshot();
+      console.log(imgSrc);
+      const blob = base64toBlob(imgSrc);
 
-              <Button variant={'outline'} className='box' onClick={userPromptScreenshot} style={{ display: 'flex', alignItems: 'center' }}>
-                <CameraIcon style={{ marginRight: '5px' }} />
-                Capture
-              </Button>
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = `${formatDate(new Date())}.png`
+      a.click();
+    }
+    // save it to downloads
 
-
-              <Button variant={isRecording ? 'destructive' : 'outline'} className='box' onClick={userPromptRecord} style={{ display: 'flex', alignItems: 'center', marginLeft: '10px' }}>
-                <VideoIcon style={{ marginRight: '5px' }} />
-                manual Record
-              </Button>
-
-              <Button style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: '10px' }} className='box'variant={autoRecordEnabled ? 'destructive' : 'outline'}
-                size={'icon'}
-                onClick={toggleAutoRecord}>
-                  Automate
-              {autoRecordEnabled?<Puff color="red" height={33}></Puff>:<PersonStanding/>}
-              </Button>
-
-              <div className="soundslider">
-              {/* <div className="icon"><Volume2Icon/></div>
-              <div className="slider"><Slider max={1} min={0} step={0.1}></Slider></div> */}
-              </div>
-            </div>  
-
-          <Separator className='2px white'/>
-          {/* <div className="flex-container">
-            <div className="flex-item"></div>
-            <div className="flex-item"></div>
-            <div className="flex-item"></div>
-          </div>
-         */}
-        {/* <Separator className='separator'/>'
-          <div>
-            
-          </div> */}
-
-        {loading && <div className='z-50 absolute w-full h-full flex items-center justify-center bg-primary-foreground'>
-          Getting things ready . . . <Rings height={50} color='red' /> 
-        </div>}  
-      {/* </div>   */}
-    </body>
-    
-    
-  );
-  function userPromptScreenshot(){
-    //add screenshot 
-
-    //save it to downloads
   }
-  function userPromptRecord(){
-    // check if recording 
-        //then stop recording
-        //save it to downloads
 
-    // if not recording
-      //start recording
+  function userPromptRecord() {
+
+    if (!webcamRef.current) {
+      toast('Camera is not found. Please refresh.')
+    }
+
+    if (mediaRecorderRef.current?.state == 'recording') {
+      // check if recording
+      // then stop recording 
+      // and save to downloads
+      mediaRecorderRef.current.requestData();
+      clearTimeout(stopTimeout);
+      mediaRecorderRef.current.stop();
+      toast('Recording saved to downloads');
+
+    } else {
+      // if not recording
+      // start recording 
+      startRecording(false);
+    }
   }
-  function toggleAutoRecord(){
-    if(autoRecordEnabled){
+
+  function startRecording(doBeep: boolean) {
+    if (webcamRef.current && mediaRecorderRef.current?.state !== 'recording') {
+      mediaRecorderRef.current?.start();
+      doBeep && beep(volume);
+
+      stopTimeout = setTimeout(() => {
+        if (mediaRecorderRef.current?.state === 'recording') {
+          mediaRecorderRef.current.requestData();
+          mediaRecorderRef.current.stop();
+        }
+
+      }, 30000);
+    }
+  }
+
+  function toggleAutoRecord() {
+    if (autoRecordEnabled) {
       setAutoRecordEnabled(false);
-      toast("AutoRecord Disabled")
+      toast('Autorecord disabled')
+      // show toast to user to notify the change
 
-    }
-    else{
+    } else {
       setAutoRecordEnabled(true);
-      toast("AutoRecord Enabled")
-
+      toast('Autorecord enabled')
+      // show toast
     }
-  }
-};
 
-export default Page;
+  }
+
+
+  // inner components
+  function RenderFeatureHighlightsSection() {
+    return <div className="text-xs text-muted-foreground">
+      <ul className="space-y-4">
+        
+        <li>
+          <strong>Horizontal Flip ↔️</strong>
+          <p>Adjust horizontal orientation.</p>
+          <Button className='h-6 w-6 my-2'
+            variant={'outline'} size={'icon'}
+            onClick={() => {
+              setMirrored((prev) => !prev)
+            }}
+          ><FlipHorizontal size={14} /></Button>
+        </li>
+        <Separator />
+        <li>
+          <strong>Take Pictures 📸</strong>
+          <p>Capture snapshots at any moment from the video feed.</p>
+          <Button
+            className='h-6 w-6 my-2'
+            variant={'outline'} size={'icon'}
+            onClick={userPromptScreenshot}
+          >
+            <Camera size={14} />
+          </Button>
+        </li>
+        <li>
+          <strong>Manual Video Recording 📽️</strong>
+          <p>Manually record video clips as needed.</p>
+          <Button className='h-6 w-6 my-2'
+            variant={isRecording ? 'destructive' : 'outline'} size={'icon'}
+            onClick={userPromptRecord}
+          >
+            <Video size={14} />
+          </Button>
+        </li>
+        <Separator />
+        <li>
+          <strong>Enable/Disable Auto Record 🚫</strong>
+          <p>
+            Option to enable/disable automatic video recording whenever
+            required.
+          </p>
+          <Button className='h-6 w-6 my-2'
+            variant={autoRecordEnabled ? 'destructive' : 'outline'}
+            size={'icon'}
+            onClick={toggleAutoRecord}
+          >
+            {autoRecordEnabled ? <Rings color='white' height={30} /> : <PersonStanding size={14} />}
+
+          </Button>
+        </li>
+
+        <li>
+          <strong>Volume Slider 🔊</strong>
+          <p>Adjust the volume level of the notifications.</p>
+        </li>
+        <li>
+          <strong>Camera Feed Highlighting 🎨</strong>
+          <p>
+            Highlights persons in{" "}
+            <span style={{ color: "#00B612" }}>green</span> and other objects in{" "}
+            <span style={{ color: "#FF0F0F" }}>red</span>.
+          </p>
+        </li>
+      </ul>
+    </div>
+  }
+}
+
+export default HomePage
 
 function resizeCanvas(canvasRef: React.RefObject<HTMLCanvasElement>, webcamRef: React.RefObject<Webcam>) {
   const canvas = canvasRef.current;
   const video = webcamRef.current?.video;
-  if (video && canvas) {
+
+  if ((canvas && video)) {
     const { videoWidth, videoHeight } = video;
-    canvas.width=videoWidth;
-    canvas.height=videoHeight;
+    canvas.width = videoWidth;
+    canvas.height = videoHeight;
   }
+}
+
+
+function formatDate(d: Date) {
+  const formattedDate =
+    [
+      (d.getMonth() + 1).toString().padStart(2, "0"),
+      d.getDate().toString().padStart(2, "0"),
+      d.getFullYear(),
+    ]
+      .join("-") +
+    " " +
+    [
+      d.getHours().toString().padStart(2, "0"),
+      d.getMinutes().toString().padStart(2, "0"),
+      d.getSeconds().toString().padStart(2, "0"),
+    ].join("-");
+  return formattedDate;
+}
+
+function base64toBlob(base64Data: any) {
+  const byteCharacters = atob(base64Data.split(",")[1]);
+  const arrayBuffer = new ArrayBuffer(byteCharacters.length);
+  const byteArray = new Uint8Array(arrayBuffer);
+
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteArray[i] = byteCharacters.charCodeAt(i);
+  }
+
+  return new Blob([arrayBuffer], { type: "image/png" }); // Specify the image type here
 }
